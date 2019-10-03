5f4304865d89c9e6b558956a1d0cc4a7b0e62b31
travis <travis@travis-ci.org>
2019-10-03 12:06:09 +0200

    Added layout/Header to main app file

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
+    <Header />
     <AddTodo />
   </div>
 </template>
 
 <script>
 import AddTodo from './components/AddTodo';
+import Header from './components/layout/Header';
+
 
 export default {
   name: "app",
   components: {
+    Header,
     AddTodo
   }
 };
