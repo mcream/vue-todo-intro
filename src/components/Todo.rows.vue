475b9b9e4345b6c1832a8d5bc515c24fa712c094
travis <travis@travis-ci.org>
2019-10-03 16:50:19 +0200

    Added new component Todo.rows

---
+++ src/components/Todo.rows.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>
+            <input type="text" name="" id="">
+            <button>X</button>
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name: "TodoRows",
+    props: ["todo"]
+}
+</script>
+<style scoped>
+
+</style>
