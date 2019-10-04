e82b6620a1dfe9382f5f932f71dc28ed41be34c7
travis <travis@travis-ci.org>
2019-10-04 22:09:21 +0200

    Updated addTodo component. Added uuiq

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,14 +1,32 @@
 <template>
     <div>
         <form>
-            <input type="text" class="">
+            <input type="text" v-model="title" class="" placeholder="Add">
             <input type="submit" class="btn">
         </form>
     </div>
 </template>
 <script>
+import uuid from 'uuid';
+
 export default {
-    
+    name: "AddTodo",
+    data(){
+        return{
+            title: ''
+        }
+    },
+    methods: {
+        addTodo(e){
+            e.preventDefault();
+            const adding = {
+                id: uuid.v4(),
+                title: this.title,
+                completed: false
+            }
+            this.$emit('add-todo', adding);
+        }
+    }
 }
 </script>
 <style scoped>
