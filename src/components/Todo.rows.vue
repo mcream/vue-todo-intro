4729e7120b27cf83536e4527bad85ca477a465ce
travis <travis@travis-ci.org>
2019-10-04 22:21:44 +0200

    Switch between branches

---
+++ src/components/Todo.rows.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="todo-rows" v-bind:class="{'completed':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{ todo.title }}
+            <button @click="$emit('del-todo', todo.id)" class="del">DELETE</button>
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name: "TodoRows",
+    props: ["todo"],
+    methods: {
+        markComplete(){
+            this.todo.completed = !this.todo.completed;
+        }
+    }
+}
+</script>
+<style scoped>
+.todo-rows{
+    background: #76a2bd;
+    padding: 20px;
+    border-bottom: 1px #ccc dotted;
+}
+.del{
+    background: #be3535;
+    color: #f5f5f5;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 10%;
+    cursor: pointer;
+    float: right;
+}
+.del:hover{
+    transition: .4s;
+    background: #d84646;
+}
+.completed{
+    color: #adadad;
+}
+</style>
